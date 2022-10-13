964f00b94e2f52493223fe6a35b8ddae8aabf38c
Valeska <valeskkitty@gmail.com>
2022-10-13 14:55:26 -0300

    aprendendoa a usar tipo Sring

---
+++ src/coursejava/introduction/TipoString.java
@@ -0,0 +1,14 @@
+package coursejava.introduction;
+
+public class TipoString {
+    public static void main(String[] args) {
+
+        //"String" no Java sempre sera texto de tamanho qualquer (com aspas dupla)
+        String textoQualquer = "Bom dia";
+        String nome;
+        String sobrenome;
+        String cpf;
+        String telefone;
+        String endereco;
+    }
+}

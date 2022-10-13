9b9f653fe1b7fdd207364c57724adc414408081a
Valeska <valeskkitty@gmail.com>
2022-10-13 14:59:43 -0300

    aprendendoa a usar estrutura for

---
+++ src/coursejava/introduction/EstruturaFor.java
@@ -0,0 +1,16 @@
+package coursejava.introduction;
+
+public class EstruturaFor {
+    public static void main(String[] args) {
+
+        // loop "for" que imprimirá os números de 0 a 5
+        for (int numero = 0; numero <= 5; numero++) {
+            System.out.println("Número atual é : " + numero);
+        }
+
+        // loop "for" que imprimirá os números de 5 a 1
+        for (int numero2 = 5; numero2 >= 0; numero2--) {
+            System.out.println("Número atual é : " + numero2);
+        }
+    }
+}

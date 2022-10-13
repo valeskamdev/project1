fc78ed0314f376c14e0ae936c34a95075b07957c
Valeska <valeskkitty@gmail.com>
2022-10-13 13:00:27 -0300

    concatenando variáveis

---
+++ src/coursejava/introduction/ConcatenacaoDeVariaveis.java
@@ -0,0 +1,19 @@
+package coursejava.introduction;
+
+public class ConcatenacaoDeVariaveis {
+    public static void main(String[] args) {
+
+        //concatenacao é igual a unir ou juntar dados, usando "+"
+        String nome = "Valeska Marques";
+        String cpf = "123.456.789.00";
+        String telefone = "12345-6789";
+        String endereco = "Brooklyn - EUA";
+        int idade = 19;
+
+        System.out.println("meu nome é " + nome + " e cpf: " +
+                cpf + " meu contato é: " + telefone + " e moro em " +
+                endereco + " e minha idade é " + idade);
+
+        //ou posso concatenar em uma variável
+    }
+}

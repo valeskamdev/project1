54d9a9068ecf89e53505ebbc69a1912d2136c577
Valeska <valeskkitty@gmail.com>
2022-10-13 14:57:27 -0300

    aprendendoa a usar tipo double

---
+++ src/coursejava/introduction/TipoDouble.java
@@ -0,0 +1,15 @@
+package coursejava.introduction;
+
+public class TipoDouble {
+    public static void main(String[] args) {
+
+        // "double" trata-se de um valor decimal, colocar "double" em todos para ficar em forma decimal
+        double nota1 = 9;
+        double nota2 = 7;
+        double nota3 = 5;
+        double nota4 = 8;
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Media das notas = " + media ); //media em forma decimal
+    }
+}

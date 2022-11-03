cbf89268c34da1f5e6011e871f27f923b015c863
valeskamdev <valeskkitty@gmail.com>
2022-11-03 18:55:11 -0300

    Thread em objeto usando Runnable

--- src/coursejava/threads/AulaThread.java
+++ src/coursejava/threads/AulaThread.java
@@ -1,40 +1,52 @@
 package coursejava.threads;
 
-import javax.swing.*;
-
 public class AulaThread {
 
     // maneira de dizer ao compilador que o método pode lançar uma exceção do tipo "InterruptedException"
     public static void main(String[] args) throws InterruptedException {
 
-        // processo paralelo envio de email
-        new Thread(() -> {  // função run() é chamada quando o thread é iniciado
+        // transformando thread's em objetos manipulaveis, usando "Runnable"
+        Thread threadEmail = new Thread(thread1);
+        threadEmail.start();
 
-            for(int pos = 0; pos < 10; pos++) {
+        Thread threadNotaFiscal = new Thread(thread2);
+        threadNotaFiscal.start();
 
-                System.out.println("Executando rotina, envio de e-mail");
-
-                try {
-                    Thread.sleep(1000);  // fazendo o programa esperar 1 segundo antes de executar a próxima linha
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }).start();
-
-        // processo paralelo envio de nota fiscal
-        new Thread(() -> {  // função run() é chamada quando o thread é iniciado
-
-            for(int pos = 0; pos < 10; pos++) {
-
-                System.out.println("Executando rotina, envio de nota fiscal");
-
-                try {
-                    Thread.sleep(2000);  // fazendo o programa esperar 1 segundo antes de executar a próxima linha
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }).start();
     }
+
+    // processo paralelo envio de nota fiscal
+    // criando uma nova thread
+    private static final Runnable thread1 = () -> {
+
+        for (int pos = 0; pos < 10; pos++) {
+
+            System.out.println("Executando rotina, envio de nota fiscal");
+
+            try {
+                Thread.sleep(2000);  // fazendo o programa esperar 1 segundo antes de executar a próxima linha
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    };
+
+    // processo paralelo envio de e-mail
+    // criando uma nova thread
+    private static final Runnable thread2 = () -> {
+
+        for (int pos = 0; pos < 10; pos++) {
+
+            System.out.println("Executando rotina, envio de e-mail");
+
+            try {
+                Thread.sleep(2000);  // fazendo o programa esperar 1 segundo antes de executar a próxima linha
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    };
+
 }
+

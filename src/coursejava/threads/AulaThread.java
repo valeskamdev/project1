0939d6829f4b1773b4e5ab164d55298f336b8e63
valeskamarques <valeskkitty@gmail.com>
2022-11-03 17:34:12 -0300

    Processamento concorrente entre duas Threads

--- src/coursejava/threads/AulaThread.java
+++ src/coursejava/threads/AulaThread.java
@@ -7,7 +7,7 @@ public class AulaThread {
     // maneira de dizer ao compilador que o método pode lançar uma exceção do tipo "InterruptedException"
     public static void main(String[] args) throws InterruptedException {
 
-        // processo paralelo
+        // processo paralelo envio de email
         new Thread(() -> {  // função run() é chamada quando o thread é iniciado
 
             for(int pos = 0; pos < 10; pos++) {
@@ -21,6 +21,20 @@ public class AulaThread {
                 }
             }
         }).start();
-        JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
+
+        // processo paralelo envio de nota fiscal
+        new Thread(() -> {  // função run() é chamada quando o thread é iniciado
+
+            for(int pos = 0; pos < 10; pos++) {
+
+                System.out.println("Executando rotina, envio de nota fiscal");
+
+                try {
+                    Thread.sleep(2000);  // fazendo o programa esperar 1 segundo antes de executar a próxima linha
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
     }
 }

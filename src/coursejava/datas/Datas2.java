8047dfd481709303defd549156390eaceb9c7ebc
valeskamdev <valeskkitty@gmail.com>
2022-12-07 17:16:03 -0300

    Calculo de dia, mes e ano com Calendar

---
+++ src/coursejava/datas/Datas2.java
@@ -0,0 +1,24 @@
+package coursejava.datas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Datas2 {
+    public static void main(String[] args) throws ParseException {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new SimpleDateFormat("yyyy-MM-dd").parse("2021-03-10"));
+        calendar.add(Calendar.DAY_OF_MONTH, 5);
+
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()));  // 2021-03-15
+
+        calendar.add(Calendar.MONTH, 2);
+
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()));  // 2021-05-15
+
+        calendar.add(Calendar.YEAR, 3);
+
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()));  // 2024-05-15
+    }
+}

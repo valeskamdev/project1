1298bcf3a6306b2c0706bc20d6a0692227e91bf7
valeskamdev <valeskkitty@gmail.com>
2022-12-07 17:02:31 -0300

    Comparando datas com after e before

--- src/coursejava/datas/Datas.java
+++ src/coursejava/datas/Datas.java
@@ -1,22 +1,23 @@
 package coursejava.datas;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Datas {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dataVencimento = simpleDateFormat.parse("2021-10-10");
+        Date dataAtual = simpleDateFormat.parse("2021-10-10");
 
-        SimpleDateFormat formatoSimples = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(formatoSimples.format(calendar.getTime()));
-
-        System.out.println("Dia do mes: " + calendar.get(Calendar.DAY_OF_MONTH));
-        System.out.println("Dia da semana: " + calendar.get(Calendar.DAY_OF_WEEK));
-        System.out.println("Hora : " + calendar.get(Calendar.HOUR_OF_DAY));
-        System.out.println("Minuto : " + calendar.get(Calendar.MINUTE));
-        System.out.println("Segundos : " + calendar.get(Calendar.SECOND));
-        System.out.println("Ano : " + calendar.get(Calendar.YEAR));
+        // comprarando datas e imprimindo resultados
+        if (dataVencimento.after(dataAtual)) {
+            System.out.println("O boleto não foi vencido");
+        } else if (dataVencimento.equals(dataAtual)) {
+            System.out.println("Ultimo dia do pagamento");
+        } else {
+            System.out.println("Boleto vencido");
+        }
     }
 }

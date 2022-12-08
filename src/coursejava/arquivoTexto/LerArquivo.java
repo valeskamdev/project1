79722192eefca3dad699540871402ca79d12f2bf
valeskamdev <valeskkitty@gmail.com>
2022-12-07 22:55:38 -0300

    Lendo arquivo e criando lista de objetos

--- src/coursejava/arquivoTexto/LerArquivo.java
+++ src/coursejava/arquivoTexto/LerArquivo.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivo {
@@ -12,14 +14,23 @@ public class LerArquivo {
         FileInputStream entradaArquivo = new FileInputStream(new File("/Users/valeskamarques/IdeaProjects/project1/src/coursejava/arquivoTexto/arquivo.txt"));
         Scanner scanner = new Scanner(entradaArquivo, StandardCharsets.UTF_8);
 
+        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+
         // verificando se existe uma próxima linha no arquivo
         while (scanner.hasNext()) {
             String linha = scanner.nextLine();
 
             // verificando se a linha não é nula e se não está vazia.
             if (linha != null && !linha.isEmpty()) {
-                System.out.println(linha);
+                String[] dados = linha.split(";");
+                Pessoa pessoa = new Pessoa();
+                pessoa.setNome(dados[0]);
+                pessoa.setEmail(dados[1]);
+                pessoa.setIdade(Integer.parseInt(dados[2]));
+
+                pessoas.add(pessoa);
             }
         }
+        pessoas.forEach(System.out::println);
     }
 }
